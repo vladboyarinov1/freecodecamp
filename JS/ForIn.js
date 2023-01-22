906d6d494eccd49b8fdc61f741ebde184bff11f5
vladboyarinov1 <97247234+vladboyarinov1@users.noreply.github.com>
2023-01-22 12:53:07 +0300

    add new task

---
+++ JS/ForIn.js
@@ -0,0 +1,19 @@
+const users = {
+    Alan: {
+        online: false
+    },
+    Jeff: {
+        online: true
+    },
+    Sarah: {
+        online: false
+    }
+}
+
+function countOnline(usersObj) {
+    // Only change code below this line
+
+    // Only change code above this line
+}
+
+console.log(countOnline(users));
